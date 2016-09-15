c46dcbdfe8f167ba26c90e8540b41dbfd6f0c8c7
TheCodedOne <dr.kcodex@gmail.com>
2016-09-15 02:46:02 +0100

    update vers

--- src/main/java/mmd/poweradvantage/mod/ModPrefs.java
+++ src/main/java/mmd/poweradvantage/mod/ModPrefs.java
@@ -4,7 +4,7 @@ public class ModPrefs {
 
     public static final String MODID = "poweradvantage";
     public static final String NAME = "Power Advantage";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "2.0.0";
     public static final String DEPS = "";
     public static final String ACC_MC = "[1.10.2,)";
     public static final String CSIDE = "mmd.poweradvantage.mod.proxy.ClientProxy";
