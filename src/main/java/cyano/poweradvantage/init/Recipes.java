94cbc77dee1ee31f0d72da0ebb39b55a4fc7b55f
Kovak <kovac1066@gmail.com>
2018-03-12 00:54:06 -0700

    fix most recipes for normal mode

--- src/main/java/cyano/poweradvantage/init/Recipes.java
+++ src/main/java/cyano/poweradvantage/init/Recipes.java
@@ -1,14 +1,18 @@
 package cyano.poweradvantage.init;
 
+import com.mcmoddev.basemetals.BaseMetals;
 import com.mcmoddev.basemetals.data.MaterialNames;
 import com.mcmoddev.lib.data.Names;
 import com.mcmoddev.lib.init.Materials;
 import com.mcmoddev.lib.registry.CrusherRecipeRegistry;
 import com.mcmoddev.lib.util.ConfigBase.Options;
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
@@ -17,6 +21,7 @@ import cyano.poweradvantage.PowerAdvantage;
 import cyano.poweradvantage.RecipeMode;
 import cyano.poweradvantage.registry.still.recipe.DistillationRecipeRegistry;
 
+@Mod.EventBusSubscriber
 public abstract class Recipes {
 
 	/*
@@ -32,10 +37,16 @@ public abstract class Recipes {
                 .setRegistryName(stack.getItem().getRegistryName()));
     }
 
+    static void addRecipe(RegistryEvent.Register<IRecipe> event, ItemStack stack, ResourceLocation location, Object... recipe) {
+        event.getRegistry().register(new ShapedOreRecipe(stack.getItem().getRegistryName(), stack, recipe)
+                .setRegistryName(location));
+    }
+
     @SubscribeEvent
     public static void init(RegistryEvent.Register<IRecipe> event) {
         if (initDone) return;
         Blocks.init();
+        System.out.println("Registering recipes");
 
         OreDictionary.registerOre("bread", net.minecraft.init.Items.BREAD);
         OreDictionary.registerOre("coal", net.minecraft.init.Items.COAL);
@@ -46,37 +57,41 @@ public abstract class Recipes {
         CrusherRecipeRegistry.addNewCrusherRecipe("potato", new ItemStack(Items.starch, 1));
         GameRegistry.addSmelting(Items.starch, new ItemStack(Items.bioplastic_ingot, 1), 0.1f);
 
-        addRecipe(event, new ItemStack(Items.rotator_tool, 1), "xx", "x*", " x", 'x', "ingotIron", '*', "sprocket");
+        addRecipe(event, new ItemStack(Items.rotator_tool, 1), "xx", "x*", " x", 'x', net.minecraft.init.Items.IRON_INGOT, '*', "sprocket");
 
-        addRecipe(event, new ItemStack(Blocks.item_conveyor, 5), "xxx", "ghg", "xxx", 'x', "plateSteel", 'g', "sprocket", 'h', net.minecraft.init.Blocks.HOPPER);
-        addRecipe(event, new ItemStack(Blocks.item_filter_block, 1), "x", "y", "z", 'y', "stone", 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
-        addRecipe(event, new ItemStack(Blocks.item_filter_food, 1), "x", "y", "z", 'y', "bread", 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
-        addRecipe(event, new ItemStack(Blocks.item_filter_fuel, 1), "x", "y", "z", 'y', "coal", 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
-        addRecipe(event, new ItemStack(Blocks.item_filter_inventory, 1), "x", "y", "z", 'y', "chest", 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
-        addRecipe(event, new ItemStack(Blocks.item_filter_ore, 1), "x", "y", "z", 'y', "ingotGold", 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
-        addRecipe(event, new ItemStack(Blocks.item_filter_plant, 1), "x", "y", "z", 'y', "treeSapling", 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
-        addRecipe(event, new ItemStack(Blocks.item_filter_smelt, 1), "x", "y", "z", 'y', "furnace", 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
+        addRecipe(event, new ItemStack(Blocks.item_conveyor, 5), "xxx", "ghg", "xxx", 'x', com.mcmoddev.basemetals.init.Blocks.getBlockByName("steel_plate"), 'g', "sprocket", 'h', net.minecraft.init.Blocks.HOPPER);
+        addRecipe(event, new ItemStack(Blocks.item_filter_block, 1), "x", "y", "z", 'y', net.minecraft.init.Blocks.STONE, 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
+        addRecipe(event, new ItemStack(Blocks.item_filter_food, 1), "x", "y", "z", 'y',net.minecraft.init.Items.BREAD, 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
+        addRecipe(event, new ItemStack(Blocks.item_filter_fuel, 1), "x", "y", "z", 'y', net.minecraft.init.Items.COAL, 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
+        addRecipe(event, new ItemStack(Blocks.item_filter_inventory, 1), "x", "y", "z", 'y', net.minecraft.init.Blocks.CHEST, 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
+        addRecipe(event, new ItemStack(Blocks.item_filter_ore, 1), "x", "y", "z", 'y', net.minecraft.init.Items.GOLD_INGOT, 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
+        addRecipe(event, new ItemStack(Blocks.item_filter_plant, 1), "x", "y", "z", 'y', net.minecraft.init.Blocks.SAPLING, 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
+        addRecipe(event, new ItemStack(Blocks.item_filter_smelt, 1), "x", "y", "z", 'y', net.minecraft.init.Blocks.FURNACE, 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
         addRecipe(event, new ItemStack(Blocks.item_filter_overflow, 1), "x", "z", 'x', Blocks.item_conveyor, 'z', net.minecraft.init.Blocks.WOODEN_PRESSURE_PLATE);
+        Block steelBars = com.mcmoddev.basemetals.init.Blocks.getBlockByName("steel_bars");
+        System.out.println(String.format("Steel bars block is: %s", steelBars.getRegistryName()));
+        addRecipe(event, new ItemStack(Blocks.steel_frame, 1), "xxx", "x x", "xxx", 'x', steelBars);
 
-        addRecipe(event, new ItemStack(Blocks.steel_frame, 1), "xxx", "x x", "xxx", 'x', "barsSteel");
-
-        addRecipe(event, new ItemStack(Blocks.storage_tank, 1), "xxx", "xpx", "xxx", 'x', "ingotPlastic", 'p', "pipe");
-        addRecipe(event, new ItemStack(Blocks.metal_storage_tank, 1), "xxx", "xpx", "xxx", 'x', "ingotSteel", 'p', "pipe");
-        addRecipe(event, new ItemStack(Blocks.fluid_drain, 1), " x ", "w#w", "ppp", 'x', "bars", 'w', "plateSteel", '#', "frameSteel", 'p', "pipe");
-        addRecipe(event, new ItemStack(Blocks.fluid_discharge, 1), "ppp", "w#w", " x ", 'x', "bars", 'w', "plateSteel", '#', "frameSteel", 'p', "pipe");
-        addRecipe(event, new ItemStack(Blocks.still, 1), "bpb", " f ", 'b', net.minecraft.init.Items.BUCKET, 'p', "pipe", 'f', net.minecraft.init.Blocks.FURNACE);
-        addRecipe(event, new ItemStack(Blocks.fluid_switch, 1), " l ", "pfp", 'l', net.minecraft.init.Blocks.LEVER, 'p', "pipe", 'f', "frameSteel");
+        addRecipe(event, new ItemStack(Blocks.storage_tank, 1), "xxx", "xpx", "xxx", 'x', Items.bioplastic_ingot, 'p', Blocks.fluid_pipe);
+        addRecipe(event, new ItemStack(Blocks.metal_storage_tank, 1), "xxx", "xpx", "xxx", 'x', com.mcmoddev.basemetals.init.Items.getItemByName("steel_ingot"), 'p', Blocks.fluid_pipe);
+        addRecipe(event, new ItemStack(Blocks.fluid_drain, 1), " x ", "w#w", "ppp", 'x', com.mcmoddev.basemetals.init.Blocks.getBlockByName("brass_bars"), 'w', com.mcmoddev.basemetals.init.Blocks.getBlockByName("steel_plate"), '#', Blocks.steel_frame, 'p', Blocks.fluid_pipe);
+        addRecipe(event, new ItemStack(Blocks.fluid_discharge, 1), "ppp", "w#w", " x ", 'x', steelBars, 'w', com.mcmoddev.basemetals.init.Blocks.getBlockByName("steel_plate"), '#', Blocks.steel_frame, 'p', Blocks.fluid_pipe);
+        addRecipe(event, new ItemStack(Blocks.still, 1), "bpb", " f ", 'b', net.minecraft.init.Items.BUCKET, 'p', Blocks.fluid_pipe, 'f', net.minecraft.init.Blocks.FURNACE);
+        addRecipe(event, new ItemStack(Blocks.fluid_switch, 1), " l ", "pfp", 'l', net.minecraft.init.Blocks.LEVER, 'p', Blocks.fluid_pipe, 'f', Blocks.steel_frame);
 
 
         // recipe modes
         if (PowerAdvantage.recipeMode == RecipeMode.NORMAL) {
             // normal means easy
-            addRecipe(event, new ItemStack(Items.sprocket, 4), " x ", "x/x", " x ", 'x', "ingotSteel", '/', "stickWood");
-            addRecipe(event, new ItemStack(Items.sprocket, 4), " x ", "x/x", " x ", 'x', "ingotSteel", '/', "rod");
+            addRecipe(event, new ItemStack(Items.sprocket, 4), " x ", "x/x", " x ", 'x', com.mcmoddev.basemetals.init.Items.getItemByName("steel_ingot"), '/', net.minecraft.init.Items.STICK);
+//            addRecipe(event, new ItemStack(Items.sprocket, 4), " x ", "x/x", " x ", 'x', com.mcmoddev.basemetals.init.Items.getItemByName("steel_ingot"), '/', net.minecraft.init.);
 
-            addRecipe(event, new ItemStack(Blocks.fluid_pipe, 6), "xxx", "   ", "xxx", 'x', "ingotIron");
-            addRecipe(event, new ItemStack(Blocks.fluid_pipe, 6), "xxx", "   ", "xxx", 'x', "ingotCopper");
-            addRecipe(event, new ItemStack(Blocks.metal_storage_tank, 1), "xxx", "xpx", "xxx", 'x', "ingotIron", 'p', "pipe");
+
+            addRecipe(event, new ItemStack(Blocks.fluid_pipe, 6), new ResourceLocation(PowerAdvantage.MODID, "fluid_pipe_copper"),
+                    "xxx", "   ", "xxx", 'x', com.mcmoddev.basemetals.init.Items.getItemByName("copper_ingot"));
+            addRecipe(event, new ItemStack(Blocks.fluid_pipe, 6), new ResourceLocation(PowerAdvantage.MODID, "fluid_pipe_iron"),
+                    "yyy", "   ", "yyy", 'y', net.minecraft.init.Items.IRON_INGOT);
+            addRecipe(event, new ItemStack(Blocks.metal_storage_tank, 1), "xxx", "xpx", "xxx", 'x', net.minecraft.init.Items.IRON_INGOT, 'p', Blocks.fluid_pipe);
         } else if (PowerAdvantage.recipeMode == RecipeMode.TECH_PROGRESSION) {
             // make things a little more complicated with tech-progression mode
             //TODO We need a setStrongHammers(boolean b) method in MMDLib Config File to fix this.
